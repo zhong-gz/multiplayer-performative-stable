0a33ffe205399609836aec1c2b3d9a88796e124a
ZHONG-GZ <510475503@qq.com>
2025-04-10 17:02:44 +0800

    Refactor code structure for improved readability and maintainability

--- jpyntbks/synthetic_strategic_prediction_self_perfor_vector.py
+++ jpyntbks/synthetic_strategic_prediction_self_perfor_vector.py
@@ -27,35 +27,22 @@ m= 100 # both players dimension of z_i
 d=2 # size of each players action
 # B=np.ones((d,1)) #np.array([[1],[1]]) #np.random.rand(d,1)
 # B=np.random.rand(d,1) ###
-B=np.random.uniform(size=(d,1))
+# B=np.random.uniform(size=(d,1))
+B = np.random.normal(0,sigma_theta,size=(d,1))
+
 # lam=[1.0,1.0]
-# lam=[0.1,0.1]
 lam=[0,0]
 
-# mu_A = 1.0
-# mu_AC = 0.5
-# mu_C = 0.8
-# A1=mu_A*scirand(1,d,density=density).A ###
-# Ac1=mu_AC*scirand(1,d,density=density).A ###
-# A2=mu_A*scirand(1,d,density=density).A ###
-# Ac2=mu_AC*scirand(1,d,density=density).A  ###
-# params={'A1':A1,'A2':A2,'Ac1':Ac1,'Ac2':Ac2} 
-# C1=mu_C*scirand(d,d,density=1).A ###
-# Cc1=mu_AC*scirand(d,d,density=1).A ###
-# C2=mu_C*scirand(d,d,density=1).A ###
-# Cc2=mu_AC*scirand(d,d,density=1).A 
-
-sigma_A = 1.25
+sigma_A = 0.5
 sigma_AC = 0.5
-sigma_C = sigma_A/n
-A1= np.random.normal(0,sigma_A,size=(1,d))
-Ac1= np.random.normal(0,sigma_AC,size=(1,d))
-A2= np.random.normal(0,sigma_A,size=(1,d))
-Ac2= np.random.normal(0,sigma_AC,size=(1,d))
-C1= np.random.normal(0,sigma_C,size=(d,d))
-C2= np.random.normal(0,sigma_C,size=(d,d))
-params={'A1':A1,'A2':A2,'Ac1':Ac1,'Ac2':Ac2,
-         'C1':C1,'C2':C2}
+sigma_C = 0.5
+A1= np.random.normal(0,np.sqrt(sigma_A),size=(1,d))
+Ac1= np.random.normal(0,np.sqrt(sigma_AC),size=(1,d))
+A2= np.random.normal(0,np.sqrt(sigma_A),size=(1,d))
+Ac2= np.random.normal(0,np.sqrt(sigma_AC),size=(1,d))
+C1= np.random.normal(0,np.sqrt(sigma_C),size=(d,d))
+C2= np.random.normal(0,np.sqrt(sigma_C),size=(d,d))
+params={'A1':A1,'A2':A2,'Ac1':Ac1,'Ac2':Ac2,'C1':C1,'C2':C2}
 
 MAXITER=1000
 ddg=ddstrategic_prediction(MAXITER=MAXITER, sigma_theta=sigma_theta,sigma_w=sigma_w,density=density,
@@ -188,7 +175,7 @@ print(f"Data saved to {file_name_npy}")
 ## Generate Plots
 # filename='./figs/vector_sp_theta_'+str(sigma_theta)+'_density_'+str(density)+'_mu_A_'+str(mu_A)+'_mu_AC_'+str(mu_AC)+'_m_'+str(m)+'.'
 # filename='./figs/ppw_mu_A_'+str(mu_A)+'_mu_AC_'+str(mu_AC)+'_m_'+str(m)+'_mu_C_'+str(mu_C)+'.'
-filename='./figs/ppw_sig_A_'+str(sigma_A)+'_sigma_AC_'+str(sigma_AC)+'_m_'+str(m)+'_sigma_C_'+str(sigma_C)+'.'
+filename='./ppw_figs/ppw_sig_A_'+str(sigma_A)+'_sigma_AC_'+str(sigma_AC)+'_m_'+str(m)+'_sigma_C_'+str(sigma_C)+'.'
 print(f"Figure plot to {filename}")
 SAVE=1
 
